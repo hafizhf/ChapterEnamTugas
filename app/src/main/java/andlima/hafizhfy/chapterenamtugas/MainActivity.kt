63a4532db3bc15c0223e4de272ad6d2903c66a71
hafizhf <hafizhfy@upi.edu>
2022-04-28 19:24:05 +0700

    MainActivity changes

--- app/src/main/java/andlima/hafizhfy/chapterenamtugas/MainActivity.kt
+++ app/src/main/java/andlima/hafizhfy/chapterenamtugas/MainActivity.kt
@@ -1,19 +1,27 @@
 package andlima.hafizhfy.chapterenamtugas
 
 import andlima.hafizhfy.chapterenamtugas.pertemuan2.BMIActivity
+import andlima.hafizhfy.chapterenamtugas.pertemuan3.AsyncTaskFilmActivity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
 import android.view.WindowManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        splash(BMIActivity::class.java)
+//        splash(BMIActivity::class.java)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            splash_container.visibility = View.GONE
+        }, 3000)
+        goToChosenApp()
     }
 
     private fun splash(cls: Class<*>) {
@@ -27,4 +35,17 @@ class MainActivity : AppCompatActivity() {
             finish()
         }, 3000)
     }
+
+    private fun goToChosenApp() {
+        btn_goto_bmi_calculator.setOnClickListener {
+            startActivity(Intent(this, BMIActivity::class.java))
+            finish()
+        }
+
+        btn_goto_asynctask_film.setOnClickListener {
+            startActivity(Intent(this, AsyncTaskFilmActivity::class.java))
+            finish()
+        }
+    }
+
 }
